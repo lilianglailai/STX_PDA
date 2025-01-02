c00772bbae347865fd5f82f8445c71bf276cbd7e
lilianglailai <139830666+lilianglailai@users.noreply.github.com>
2025-01-02 16:26:26 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -4,19 +4,106 @@ export default {
     // console.log('App Launch')
   },
   onShow: function () {
-    // console.log('App Show')
+
   },
   onHide: function () {
     // console.log('App Hide')
-  }
+  },
+  methods: {
+    update() {
+      this.apifn({
+        url: `pad/v1/client/appUpdate/newObject`,  //请求接口
+
+      }).then(res => {
+        let versions = res.data.versionName
+        let apkUrl = res.data.apkUrl
+        plus.runtime.getProperty(plus.runtime.appid, (inf) => {
+          console.log(inf.version,versions);
+          
+          if (inf.version != versions) {
+            uni.showModal({
+              title: this.$t('updateFn.title'),
+              content: this.$t('updateFn.Download'),
+              success: (res) => {
+                if (res.confirm == true) {//当用户确定更新，执行更新
+                  this.doUpData(apkUrl);
+                }
+              }
+            })
+          }
+        });
+      },
+        err => {
+          console.log(err);
+
+        }
+      )
+    },
+
+    doUpData(url) {
+
+      let downloadTask = uni.downloadFile({//执行下载
+        url: url, //下载地址
+        success: downloadResult => {//下载成功
+          uni.hideLoading();
+          if (downloadResult.statusCode == 200) {
+            uni.showModal({
+              title: '',
+              content: this.$t('updateFn.UpdateSuccess'),
+              confirmText: this.$t('updateFn.restart'),
+              confirmColor: '#EE8F57',
+              success: function (res) {
+                if (res.confirm == true) {
+                  plus.runtime.install(//安装
+                    downloadResult.tempFilePath, {
+                    force: true
+                  },
+                    function (res) {
+                      utils.showToast(this.$t('updateFn.Restarting'));
+                      plus.runtime.restart();
+                    }
+                  );
+                }
+              }
+            });
+          }
+        },
+        fail: err => {
+          uni.hideLoading()
+          uni.showToast({
+            title: this.$t('err'),
+            icon: "none",
+          });
+        }
+      });
+      var showLoading = plus.nativeUI.showWaiting(this.$t('updateFn.updating'), { back: "none" });//{back: "none"}用来禁止返回键
+      downloadTask.onProgressUpdate((res) => {
+        showLoading.setTitle(this.$t('updateFn.downloading') + res.progress + "%  ");
+        if (res.progress == 100) {
+          plus.nativeUI.closeWaiting();
+        }
+
+      });
+    }
+
+
+  },
+  created() {
+    // #ifdef APP 
+    this.update()
+    // #endif 
+  },
+
 }
 </script>
 
 <style lang="scss">
 @import "@/static/ll_css.scss";
-*{
+
+* {
   box-sizing: border-box;
 }
+
 /*每个页面公共css */
 .router-box {
   padding-top: calc(var(--status-bar-height) + 131.6rpx)
@@ -25,27 +112,30 @@ export default {
 .app-box {
   margin-top: 10px;
 }
-.highlight{
-  color:  #339f69 !important;
+
+.highlight {
+  color: #339f69 !important;
 }
-.scan-input { 
+
+.scan-input {
   .is-input-border {
     border: unset !important;
   }
 
-   .uni-easyinput__content-input {
+  .uni-easyinput__content-input {
     height: 80rpx !important;
     background: #ffffff;
     font-size: 38rpx !important;
   }
 }
+
 .scan-submit {
-    height: 80rpx;
-    line-height: 80rpx;
-    margin-left: 20rpx;
-    background-color: #3882ee;
-    border-radius: 40rpx;
-    color: white;
+  height: 80rpx;
+  line-height: 80rpx;
+  margin-left: 20rpx;
+  background-color: #3882ee;
+  border-radius: 40rpx;
+  color: white;
 }
 
 body {
@@ -81,7 +171,8 @@ body {
     color: #ffffff;
   }
 }
-.as-select-popup-content{
+
+.as-select-popup-content {
   height: 70vh;
 }
 </style>
